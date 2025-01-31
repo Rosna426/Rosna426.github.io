928f637a1611afe297e93f9f73e7107a17111021
Rosna426 <rosnapr206@gmail.com>
2025-01-31 14:26:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function calculate(){
+    inputElement=document.getElementById("sample").value;
+    answer=eval(inputElement)
+    alert(answer)
+    document.getElementById("sample").value=answer
+}
